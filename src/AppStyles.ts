d051f11dbd101e44931806360af3e6aeb3242cf1
Hadrian Hughes <h.hughes@amaze.com>
2020-04-12 23:29:08 +0100

    Add a row gap at mobile breakpoints

--- src/AppStyles.ts
+++ src/AppStyles.ts
@@ -7,8 +7,10 @@ export const Wrapper = styled.div`
   grid-template-columns: 1fr;
   margin: 0 auto;
   max-width: ${rem(appMaxWidth)};
+  grid-row-gap: ${rem(10)};
 
   ${minWidth('medium')} {
     grid-template-columns: repeat(2, 1fr);
+    grid-row-gap: 0;
   }
 `;
