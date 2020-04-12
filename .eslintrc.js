cc38f566c5889cdfe6727d261cb3309555e6e1c5
Hadrian Hughes <h.hughes@amaze.com>
2020-04-12 16:28:27 +0100

    Ban console.log

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended']
+  extends: ['plugin:@typescript-eslint/recommended'],
+  rules: {
+    'no-console': [2, { allow: ['warn', 'error', 'info'] }],
+  }
 };
